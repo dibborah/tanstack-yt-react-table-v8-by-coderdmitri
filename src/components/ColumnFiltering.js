151f2a19b1a38bb274875f044fb255c38975150b
dibborah <dibyajyotiborah469@gmail.com>
2024-03-05 18:24:07 +0530

    done columnFilter

--- src/components/ColumnFiltering.js
+++ src/components/ColumnFiltering.js
@@ -1,19 +1,19 @@
+import React from "react";
+import "./tables.css";
 import {
-  flexRender,
   useReactTable,
+  flexRender,
   getCoreRowModel,
   getFilteredRowModel,
 } from "@tanstack/react-table";
 import { columnDef } from "./columns";
-import "./tables.css";
-import React from "react";
 import dataJSON from "./data.json";
 import FilterFunction from "./FilterFunction";
 
-const BasicTables = () => {
+const ColumnFiltering = () => {
   const finalData = React.useMemo(() => dataJSON, []);
   const finalColumnDef = React.useMemo(() => columnDef, []);
-  const [columnFilter, setColumnFilter] = React.useState("");
+  const [columnFilters, setColumnFilters] = React.useState([]);
 
   const tableInstance = useReactTable({
     columns: finalColumnDef,
@@ -21,74 +21,71 @@ const BasicTables = () => {
     getCoreRowModel: getCoreRowModel(),
     getFilteredRowModel: getFilteredRowModel(),
     state: {
-      columnFilters: columnFilter,
+      columnFilters: columnFilters,
     },
-    onColumnFiltersChange: setColumnFilter,
+    onColumnFiltersChange: setColumnFilters,
   });
-  // console.log("test body row", tableInstance.getRowModel().rows);
+
+  //   console.log("test", tableInstance.getHeaderGroups());
+
+//   React.useEffect(() => {
+//     console.log(tableInstance.getState().columnFilters);
+//   });
+
   return (
-    <table>
-      <thead>
-        {tableInstance.getHeaderGroups().map((headerEl) => {
-          return (
-            <tr key={headerEl.id}>
-              {headerEl.headers.map((columnEl) => {
-                return (
-                  <th key={columnEl.id} colSpan={columnEl.colSpan}>
-                    {columnEl.isPlaceholder ? null : (
-                      <span>
-                        {flexRender(
-                          columnEl.column.columnDef.header,
-                          columnEl.getContext()
-                        )}
-                        <div><FilterFunction column={columnEl.column} table={tableInstance}/></div>
-                      </span>
-                    )}
-                  </th>
-                );
-              })}
-            </tr>
-          );
-        })}
-      </thead>
-      <tbody>
-        {tableInstance.getRowModel().rows.map((rowEl) => {
-          return (
-            <tr key={rowEl.id}>
-              {rowEl.getVisibleCells().map((cellEl) => {
-                return (
-                  <td key={cellEl.id}>
-                    {flexRender(
-                      cellEl.column.columnDef.cell,
-                      cellEl.getContext()
-                    )}
-                  </td>
-                );
-              })}
-            </tr>
-          );
-        })}
-      </tbody>
-      <tfoot>
-        {tableInstance.getHeaderGroups().map((headerEl) => {
-          return (
-            <tr key={headerEl.id}>
-              {headerEl.headers.map((columnEl) => {
-                return (
-                  <th key={columnEl.id} colSpan={columnEl.colSpan}>
-                    {flexRender(
-                      columnEl.column.columnDef.header,
-                      columnEl.getContext()
-                    )}
-                  </th>
-                );
-              })}
-            </tr>
-          );
-        })}
-      </tfoot>
-    </table>
+    <>
+      <table>
+        <thead>
+          {tableInstance.getHeaderGroups().map((headerEl) => {
+            return (
+              <tr key={headerEl.id}>
+                {headerEl.headers.map((columnEl) => {
+                  return (
+                    <th key={columnEl.id} colSpan={columnEl.colSpan}>
+                      {columnEl.isPlaceholder ? null : (
+                        <>
+                          {flexRender(
+                            columnEl.column.columnDef.header,
+                            columnEl.getContext()
+                          )}
+                          {columnEl.column.getCanFilter() ? (// This line which is not in the video but is in the repo is very very important
+                            <div>
+                              <FilterFunction
+                                column={columnEl.column}
+                                table={tableInstance}
+                              />
+                            </div>
+                          ) : null}
+                        </>
+                      )}
+                    </th>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </thead>
+        <tbody>
+          {tableInstance.getRowModel().rows.map((rowEl) => {
+            return (
+              <tr key={rowEl.id}>
+                {rowEl.getVisibleCells().map((cellEl) => {
+                  return (
+                    <td key={cellEl.id}>
+                      {flexRender(
+                        cellEl.column.columnDef.cell,
+                        cellEl.getContext()
+                      )}
+                    </td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </>
   );
 };
 
-export default BasicTables;
+export default ColumnFiltering;
